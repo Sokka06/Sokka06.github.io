1ca3d8c539cb8ffa94adae8f519498a4f49e0b99
Sokka06 <Sokka06@gmail.com>
2022-03-03 14:26:34 +0200

    only list owned github repos

--- src/github-api.js
+++ src/github-api.js
@@ -1,8 +1,10 @@
+// https://gist.github.com/MichaelCurrin/72dea64bad7e285323865788aa5871f6
+
 exports.githubApiQuery = `
 query($github_login: String!) {
     user(login: $github_login) {
       name
-      repositories(first: 10, privacy: PUBLIC, orderBy: {field: CREATED_AT, direction: DESC}) {
+      repositories(first: 10, ownerAffiliations: OWNER, privacy: PUBLIC, orderBy: {field: CREATED_AT, direction: DESC}) {
         nodes {
 			id
         	name
